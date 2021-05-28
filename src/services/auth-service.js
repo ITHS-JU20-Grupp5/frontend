39dc644d8b903f50a3e2be407fd2ed3884ec4edd
Tobias <twadseth@icloud.com>
2021-05-28 14:01:18 +0200

    Reverted link back to prod

--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/auth/';
+const API_URL = 'https://generalknowledge.azurewebsites.net/auth/';
 
 class AuthService {
 	login(user) {
