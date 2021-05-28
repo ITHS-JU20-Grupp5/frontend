4cd904e422bd348435388c1cbf6b1c4552cca85a
Tobias <twadseth@icloud.com>
2021-05-28 13:55:42 +0200

    Updated admin panel

--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://generalknowledge.azurewebsites.net/auth/';
+const API_URL = 'http://localhost:3000/auth/';
 
 class AuthService {
 	login(user) {
